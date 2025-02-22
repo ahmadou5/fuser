e08b2ed4b45d9e84be9dbb089aca9f08e52e4008
ahmadou5 <ahmadlasauwal@gmail.com>
2025-02-23 00:53:13 +0100

    chore:enviromrnt

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,11 +1,11 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import mongoose from "mongoose";
 
-if (!process.env.NEXT_PUBLIC_MONGODB_URI) {
+if (!process.env.MONGODB_URI) {
   throw new Error("Please define the MONGODB_URI environment variable");
 }
 
-const MONGODB_URI = process.env.NEXT_PUBLIC_MONGODB_URI;
+const MONGODB_URI = process.env.MONGODB_URI;
 
 let cached = (global as any).mongoose;
 
