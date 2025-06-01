953362070558ac883851000ade557b5fb558a903
ahmadou5 <ahmadlasauwal@gmail.com>
2025-06-01 05:20:07 +0100

    update usdc logo

--- src/app/api/solana/tokens/route.ts
+++ src/app/api/solana/tokens/route.ts
@@ -33,12 +33,14 @@ const KNOWN_TOKENS: {
   Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB: {
     name: "Tether USD",
     symbol: "USDT",
-    image: "https://cryptologos.cc/logos/tether-usdt-logo.png",
+    image:
+      "https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB/logo.svg",
   },
   EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v: {
     name: "USD Coin",
     symbol: "USDC",
-    image: "https://cryptologos.cc/logos/usd-coin-usdc-logo.png",
+    image:
+      "https://statics.solscan.io/cdn/imgs/s60?ref=68747470733a2f2f7261772e67697468756275736572636f6e74656e742e636f6d2f736f6c616e612d6c6162732f746f6b656e2d6c6973742f6d61696e2f6173736574732f6d61696e6e65742f45506a465764643541756671535371654d32714e31787a7962617043384734774547476b5a777954447431762f6c6f676f2e706e67",
   },
   So11111111111111111111111111111111111111112: {
     name: "Wrapped SOL",
